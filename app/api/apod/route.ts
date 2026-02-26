7bc38dd8d4ef9aa856518cc598f1c1b1238f5d0f
Tommy Evertsen <tommyevertsen52@gmail.com>
2026-02-26 14:03:53 +0100

    Added APOD image

--- app/api/apod/route.ts
+++ app/api/apod/route.ts
@@ -2,22 +2,26 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   const API_KEY = process.env.API_KEY;
+  const startTime = Date.now();
 
   try {
     const response = await fetch(
       `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`,
       {
         cache: "force-cache",
-        next: { revalidate: 300 },
+        next: { revalidate: 86400 },
       },
     );
 
     const data = await response.json();
+    const endTime = Date.now();
+    console.log(`APOD API response time: ${endTime - startTime}ms`);
+
     return NextResponse.json(data);
   } catch (error) {
     console.log("could not fetch apod api", error);
     return NextResponse.json(
-      { error: "Failed to fetch popular games" },
+      { error: "Failed to fetch APOD data" },
       { status: 500 },
     );
   }
